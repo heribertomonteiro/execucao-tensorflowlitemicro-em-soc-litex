3b57abe1584e967ef78afdffd5cd6a77a650143a
heribertomonteiro <heribertomonteiro@users.noreply.github.com>
2025-11-26 11:20:25 -0500

    Add build directories: firmware/build and firmware/tflm/build

---
+++ firmware/build/crt0.d
@@ -0,0 +1,4 @@
+build/crt0.o: \
+ /home/osboxes/Documents/litex/litex/litex/soc/cores/cpu/picorv32/crt0.S \
+ /home/osboxes/Documents/litex/litex/litex/soc/cores/cpu/picorv32/extraops.S
+/home/osboxes/Documents/litex/litex/litex/soc/cores/cpu/picorv32/extraops.S:
